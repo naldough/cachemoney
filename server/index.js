58e397e84137af1cfdcf3e63c6ef6c651e9d9ef0
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2022-03-01 11:51:15 -0800

    index.js api

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const db = require('./db');
 const itemRouter = require('./routes/item-router');
 
 const app = express();
-const apiPort = 3000;
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+    hostname: process.env.REEACT_APP_API_HOST || 'http://localhost:3000/',
+    httpsAgent: https.Agent({
+      rejectUnauthorized: false,
+    }),
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +25,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// app.use('/api', itemRouter);
 app.use('/api', itemRouter);
 
-app.listen(apiPort, () => {
+// The order of routes are important
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+app.listen(api, () => {
     console.log(`[Hack.Diversity React Template] - Server running on port ${apiPort}`);
 });
